1a3fe739bbe7b1ed6e1458448b667d357b05f1ad
Ashish Rathore <ashrathore98@gmail.com>
2023-09-05 01:57:17 +0530

    first commit

---
+++ AssistedProject_13/Controllers/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Phase3HTMLForms.Controllers
+{
+    internal class ErrorViewModel : IView
+    {
+        public object RequestId { get; set; }
+    }
+}
---
+++ AssistedProject_13/Controllers/ResponseCacheAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Phase3HTMLForms.Controllers
+{
+    internal class ResponseCacheAttribute : Attribute
+    {
+        public int Duration { get; set; }
+        public object Location { get; set; }
+        public bool NoStore { get; set; }
+    }
+}
